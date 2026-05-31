c0ba4ee96cb2b8054185ffbbba153b9695f564b2
rptsd3132 <rpt.sandeepa.dilhara@gmail.com>
2026-05-31 11:27:55 +0530

    setup the project and set the data base

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello! My backend is working.');
+});
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Database connected! Time is: ${result.rows[0].now}`);
+  } catch (err) {
+    res.send(`Database error: ${err.message}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
